4002296358734d9e36e158c2c32dd9137b924fc1
raberin <rmvaberin@gmail.com>
2019-06-30 12:08:34 -0700

    Display open/close and lock/unlock is displayed tests

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,36 @@
 // Test away!
+import React from "react";
+import renderer from "react-test-renderer"; // 1: install this npm module as a dev dependency
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/react/cleanup-after-each";
+import Dashboard from "../dashboard/Dashboard";
+
+describe("<Display/>", () => {
+  it("displays open and closed", () => {
+    const { getByText } = render(<Dashboard />);
+
+    //Tests if door is open
+    getByText(/open/i);
+
+    const closeButton = getByText(/close gate/i);
+    fireEvent.click(closeButton);
+
+    //Test if door is closed
+    getByText(/closed/i);
+  });
+
+  it("displays locked and unlocked", () => {
+    const { getByText } = render(<Dashboard />);
+
+    //Tests if door is unlocked
+    getByText(/unlocked/i);
+
+    const closeButton = getByText(/close gate/i);
+    const lockButton = getByText(/lock gate/i);
+
+    fireEvent.click(closeButton);
+    fireEvent.click(lockButton);
+
+    getByText(/locked/i);
+  });
+});
